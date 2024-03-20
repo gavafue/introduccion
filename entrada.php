6b786e5e164aa2936cfe8f983f6911618543edb5
Gabriel Vazquez <gavf.vazquez@gmail.com>
2024-03-20 08:23:36 -0300

    Creacion de mi primer archivo php

---
+++ entrada.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>entrada.html</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <form action="salida.php" method="post">
+        <p>Su nombre: <input type="text" name="nombre" /></p>
+        <p>Su edad: <input type="text" name="edad" /></p>
+        <p><input type="submit" /></p>
+    </form>
+</body>
+
+</html>
